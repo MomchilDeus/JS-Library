ba0e7ba468d1bc4f438e878d655f6a46c7862633
MomchilDeus <momchil.kolev@protonmail.com>
2018-07-01 09:50:23 +0300

    deepEqual - compare objects' values instead of identity

---
+++ deepEqual.js
@@ -0,0 +1,8 @@
+function deepEqual(obj1, obj2) {
+  for (let prop in obj1) {
+    if (typeof obj1[prop] == 'object' && typeof obj2[prop] == 'object') deepEqual(obj1[prop], obj2[prop])
+    else if (obj1[prop] === obj2[prop]) continue
+    else return false
+  }
+  return true
+}
